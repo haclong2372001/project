c36b711372394ec142f6e19758e06ea00d89e342
haclong2372001 <phanlong5244@gmail.com>
2023-05-13 12:21:24 +0700

    showlist

---
+++ src/router/router.jsx
@@ -0,0 +1,24 @@
+import { createBrowserRouter, createRoutesFromElements, Route } from 'react-router-dom'
+import PageHome from '@Pages/PageHome';
+import PageProducts from '@Pages/PageProducts';
+import PageProductDetails from '@Pages/PageProductDetails';
+import MainMenu from '@Layout/MainMenu';
+
+
+export const routes = [
+  { title: 'Home', path: '/', component: PageHome, isShowMenu: true },
+  { title: 'Products', path: '/products', component: PageProducts, isShowMenu: true },
+  { title: 'Details Product', path: '/product/:id', component: PageProductDetails, isShowMenu: false },
+  
+]
+
+const router = createBrowserRouter(createRoutesFromElements(
+  <Route path='/' element={<MainMenu />}>
+    {routes.map(i =>
+      <Route key={i.path} path={i.path} element={<i.component />} />)
+    }
+  </Route>
+), { basename: "/react-deploy" });
+
+
+export { router }
